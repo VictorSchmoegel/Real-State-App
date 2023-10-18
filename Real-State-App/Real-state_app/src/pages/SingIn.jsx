e8b9392d642ad47f7ed70d8c063dce747f665962
Victor Schmoegel <victorschmoegel@gmail.com>
2023-10-18 17:05:00 -0300

    criado o botão de login com google e sua funcionalidade

--- Real-State-App/Real-state_app/src/pages/SingIn.jsx
+++ Real-State-App/Real-state_app/src/pages/SingIn.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { singInStart, singInSuccess, singInFailure } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import OAuth from '../components/OAuth';
 
 export default function SingIn() {
   const [formData, setFormData] = useState({});
@@ -64,6 +65,7 @@ export default function SingIn() {
         >
           {loading ? 'Loading...' : 'Sing In'}
         </button>
+        <OAuth />
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Dont have an account?</p>
