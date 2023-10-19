0693a5dddbcb4e6f30a29c72fea876e5aeab8f63
Victor Schmoegel <victorschmoegel@gmail.com>
2023-10-19 15:05:38 -0300

    adicionado botão e os textos para opções de conta

--- Real-State-App/Real-state_app/src/pages/Profile.jsx
+++ Real-State-App/Real-state_app/src/pages/Profile.jsx
@@ -25,7 +25,16 @@ export default function Profile() {
           placeholder='password'
           id='password'
         />
+        <button
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          Update
+        </button>
       </form>
+      <div className='flex justify-between mt-3'>
+        <span className='text-red-700 cursor-pointer '>Delete Account</span>
+        <span className='text-red-700 cursor-pointer '>Sing Out</span>
+      </div>
     </div>
   )
 }
