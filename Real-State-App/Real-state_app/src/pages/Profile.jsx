de9eefabe153123c29607a4f018a373550eeb807
Victor Schmoegel <victorschmoegel@gmail.com>
2023-10-19 15:00:05 -0300

    criando inputs e adicionando foto de perfil na pag profile

--- Real-State-App/Real-state_app/src/pages/Profile.jsx
+++ Real-State-App/Real-state_app/src/pages/Profile.jsx
@@ -1,5 +1,31 @@
+import { useSelector } from "react-redux"
+
 export default function Profile() {
+  const { currentUser } = useSelector(state => state.user)
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'  src={currentUser.photo} alt="foto do perfil" />
+        <input
+          className='border p-3 rounded-lg'
+          type="text"
+          placeholder='Username'
+          id='username'
+        />
+        <input
+          className='border p-3 rounded-lg'
+          type="email"
+          placeholder='email'
+          id='email'
+        />
+        <input
+          className='border p-3 rounded-lg'
+          type="password"
+          placeholder='password'
+          id='password'
+        />
+      </form>
+    </div>
   )
 }
