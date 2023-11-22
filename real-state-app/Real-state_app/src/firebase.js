9777658135eae56c634fb347eb0e7a61effe661c
victor schmoegel <victorschmoegel@gmail.com>
2023-11-22 12:05:35 -0300

    trocando de pc

---
+++ real-state-app/Real-state_app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgRTSHkP_eEwRXtFdColLtanMhHa5oXB0',
+  authDomain: "realstate-93a30.firebaseapp.com",
+  projectId: "realstate-93a30",
+  storageBucket: "realstate-93a30.appspot.com",
+  messagingSenderId: "238621527821",
+  appId: "1:238621527821:web:0ef98b68de8a560ad0ea44"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
