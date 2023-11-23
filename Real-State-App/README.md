490d7772bccea381bf4e259d9c83fc04a183289e
victor schmoegel <victorschmoegel@gmail.com>
2023-11-23 11:20:28 -0300

    deleting duplicate files

--- Real-State-App/README.md
+++
@@ -1,2 +0,0 @@
-# Real-State-App
-Fullstack MERN real state application
--- Real-State-App/api/routes/listing.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createList } from '../controllers/listing.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
-
-const router = express.Router();
-
-router.post('/create',verifyToken , createList);
-
-export default router;
