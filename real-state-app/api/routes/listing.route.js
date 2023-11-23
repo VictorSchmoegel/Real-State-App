24d053b9b8efdf91509131768733b803edac4e08
victor schmoegel <victorschmoegel@gmail.com>
2023-11-23 13:35:09 -0300

    create update list functionality

--- real-state-app/api/routes/listing.route.js
+++ real-state-app/api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createList, deleteList, updateList } from '../controllers/listing.controller.js';
+import { createList, deleteList, updateList, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createList);
 router.delete('/delete/:id', verifyToken, deleteList);
 router.post('/update/:id', verifyToken, updateList);
+router.get('/get/:id', getListing);
 
 export default router;
