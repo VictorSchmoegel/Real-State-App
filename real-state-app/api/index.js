fc5fec81ee4d0535cae6d391fac5beeae06b514d
victor schmoegel <victorschmoegel@gmail.com>
2023-11-28 14:17:02 -0300

    config node to vercel2

--- real-state-app/api/index.js
+++ real-state-app/api/index.js
@@ -8,12 +8,14 @@ import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log('MongoDB Connected!!');
-}).catch((err) => {
-  console.log(`${err} MongoDB Connection Error!!}`);
-});
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const __dirname = path.resolve();
 
@@ -24,12 +26,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!!');
+  console.log('Server is running on port 3000!');
 });
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
