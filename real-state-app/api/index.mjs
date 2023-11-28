9c3e16d120902c67e29269164e64709b409d8b0f
victor schmoegel <victorschmoegel@gmail.com>
2023-11-28 19:08:27 -0300

    ajuda senhor

--- real-state-app/api/index.mjs
+++ real-state-app/api/index.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+// import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
@@ -7,6 +7,8 @@ import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
+
+const express = require('express')
 
 mongoose
   .connect(process.env.MONGO)
